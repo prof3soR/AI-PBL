2fcf0c07ed78a3af807ed41272aeab7fafdd80a8
Rohit kumar <76482418+prof3soR@users.noreply.github.com>
2022-11-03 23:19:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,207 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+import numpy as np
+import io
+import json
+movies = pd.read_csv('credits.csv')
+credits = pd.read_csv('movies.csv')
+# changing the genres column from json to string
+movies['genres'] = movies['genres'].apply(json.loads)
+for index,i in zip(movies.index,movies['genres']):
+    list1 = []
+    for j in range(len(i)):
+        list1.append((i[j]['name'])) # the key 'name' contains the name of the genre
+    movies.loc[index,'genres'] = str(list1)
+
+# changing the keywords column from json to string
+movies['keywords'] = movies['keywords'].apply(json.loads)
+for index,i in zip(movies.index,movies['keywords']):
+    list1 = []
+    for j in range(len(i)):
+        list1.append((i[j]['name']))
+    movies.loc[index,'keywords'] = str(list1)
+    
+# changing the production_companies column from json to string
+movies['production_companies'] = movies['production_companies'].apply(json.loads)
+for index,i in zip(movies.index,movies['production_companies']):
+    list1 = []
+    for j in range(len(i)):
+        list1.append((i[j]['name']))
+    movies.loc[index,'production_companies'] = str(list1)
+
+# changing the cast column from json to string
+credits['cast'] = credits['cast'].apply(json.loads)
+for index,i in zip(credits.index,credits['cast']):
+    list1 = []
+    for j in range(len(i)):
+        list1.append((i[j]['name']))
+    credits.loc[index,'cast'] = str(list1)
+
+# changing the crew column from json to string    
+credits['crew'] = credits['crew'].apply(json.loads)
+def director(x):
+    for i in x:
+        if i['job'] == 'Director':
+            return i['name']
+credits['crew'] = credits['crew'].apply(director)
+credits.rename(columns={'crew':'director'},inplace=True)
+movies = movies.merge(credits,left_on='id',right_on='movie_id',how='left')
+movies = movies[['id','original_title','genres','cast','vote_average','director','keywords']]
+MoviesList=[]
+for i in movies['original_title']:
+    if i not in MoviesList:
+        MoviesList.append(i)
+movies['genres'] = movies['genres'].str.strip('[]').str.replace(' ','').str.replace("'",'')
+movies['genres'] = movies['genres'].str.split(',')
+for i,j in zip(movies['genres'],movies.index):
+    list2=[]
+    list2=i
+    list2.sort()
+    movies.loc[j,'genres']=str(list2)
+movies['genres'] = movies['genres'].str.strip('[]').str.replace(' ','').str.replace("'",'')
+movies['genres'] = movies['genres'].str.split(',')
+genreList = []
+for index, row in movies.iterrows():
+    genres = row["genres"]
+    
+    for genre in genres:
+        if genre not in genreList:
+            genreList.append(genre)
+def binary(genre_list):
+    binaryList = []
+    
+    for genre in genreList:
+        if genre in genre_list:
+            binaryList.append(1)
+        else:
+            binaryList.append(0)
+    
+    return binaryList
+movies['genres_bin'] = movies['genres'].apply(lambda x: binary(x))
+castList = []
+for index, row in movies.iterrows():
+    cast = row["cast"]
+    
+    for i in cast:
+        if i not in castList:
+            castList.append(i)
+def binary(cast_list):
+    binaryList = []
+    
+    for genre in castList:
+        if genre in cast_list:
+            binaryList.append(1)
+        else:
+            binaryList.append(0)
+    
+    return binaryList
+movies['cast_bin'] = movies['cast'].apply(lambda x: binary(x))
+def binary(cast_list):
+    binaryList = []
+    
+    for genre in castList:
+        if genre in cast_list:
+            binaryList.append(1)
+        else:
+            binaryList.append(0)
+    
+    return binaryList
+def xstr(s):
+    if s is None:
+        return ''
+    return str(s)
+movies['director'] = movies['director'].apply(xstr)
+directorList=[]
+for i in movies['director']:
+    if i not in directorList:
+        directorList.append(i)
+movies['director_bin'] = movies['director'].apply(lambda x: binary(x))
+movies['keywords'] = movies['keywords'].str.strip('[]').str.replace(' ','').str.replace("'",'').str.replace('"','')
+movies['keywords'] = movies['keywords'].str.split(',')
+for i,j in zip(movies['keywords'],movies.index):
+    list2 = []
+    list2 = i
+    movies.loc[j,'keywords'] = str(list2)
+movies['keywords'] = movies['keywords'].str.strip('[]').str.replace(' ','').str.replace("'",'')
+movies['keywords'] = movies['keywords'].str.split(',')
+for i,j in zip(movies['keywords'],movies.index):
+    list2 = []
+    list2 = i
+    list2.sort()
+    movies.loc[j,'keywords'] = str(list2)
+movies['keywords'] = movies['keywords'].str.strip('[]').str.replace(' ','').str.replace("'",'')
+movies['keywords'] = movies['keywords'].str.split(',')
+movies['words_bin'] = movies['keywords'].apply(lambda x: binary(x))
+movies = movies[(movies['vote_average']!=0)] 
+movies = movies[movies['director']!='']
+from scipy import spatial
+
+def Similarity(movieId1, movieId2):
+    a = movies.iloc[movieId1]
+    b = movies.iloc[movieId2]
+    
+    genresA = a['genres_bin']
+    genresB = b['genres_bin']
+    
+    genreDistance = spatial.distance.cosine(genresA, genresB)
+    
+    scoreA = a['cast_bin']
+    scoreB = b['cast_bin']
+    scoreDistance = spatial.distance.cosine(scoreA, scoreB)
+    
+    directA = a['director_bin']
+    directB = b['director_bin']
+    directDistance = spatial.distance.cosine(directA, directB)
+    
+    wordsA = a['words_bin']
+    wordsB = b['words_bin']
+    wordsDistance = spatial.distance.cosine(directA, directB)
+    return genreDistance + directDistance + scoreDistance + wordsDistance
+new_id = list(range(0,movies.shape[0]))
+movies['new_id']=new_id
+movies=movies[['original_title','genres','vote_average','genres_bin','cast_bin','id','new_id','director','director_bin','words_bin']]
+import requests as r
+def poster(id):
+  Movie_data=r.get('https://api.themoviedb.org/3/movie/{}?api_key=692ef1307f53ce56d5b22b40fdd666c4&language=en-US'.format(id)).json()
+  return "https://image.tmdb.org/t/p/w500/"+Movie_data['poster_path']
+import operator
+def predict_score(name):
+    new_movie = movies[movies['original_title'].str.contains(name)].iloc[0].to_frame().T
+    print('Selected Movie: ',new_movie.original_title.values[0])
+    def getNeighbors(baseMovie, K):
+        distances = []
+    
+        for index, movie in movies.iterrows():
+            if movie['new_id'] != baseMovie['new_id'].values[0]:
+                dist = Similarity(baseMovie['new_id'].values[0], movie['new_id'])
+                distances.append((movie['new_id'], dist))
+    
+        distances.sort(key=operator.itemgetter(1))
+        neighbors = []
+    
+        for x in range(K):
+            neighbors.append(distances[x])
+        return neighbors
+
+    K = 10
+    neighbors = getNeighbors(new_movie, K)
+    
+    print('\nRecommended Movies: \n')
+
+    for neighbor in neighbors:
+        st.image(poster(movies.iloc[neighbor[0]][5]))
+        st.write(movies.iloc[neighbor[0]][0]," | Genres: ",str(movies.iloc[neighbor[0]][1]).strip('[]').replace(' ','')," | Rating: ",str(movies.iloc[neighbor[0]][2]))
+st.title("Movie suggestion page PBL")
+
+
+name= st.selectbox(
+     'Hey there! please enter the title of your favorite movie : ',MoviesList)
+
+st.write('You selected:', name)
+if st.button('Suggest'):
+     predict_score(name)
+
+
